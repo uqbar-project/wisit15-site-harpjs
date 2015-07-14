21f8b2ac09afaa07cfbb94cbb852fc7af89ca8ec
Nicolás Passerini <npasserini@gmail.com>
2015-07-14 10:52:53 -0300

    Corrected which files are to be ignored

--- deployDev.sh
+++ deployDev.sh
@@ -9,8 +9,15 @@ ORIGIN_URL_WITH_CREDENTIALS="https://${GITHUB_TOKEN}@${ORIGIN_URL}"
 echo "Compiling site into " ${TEMP_DIRECTORY}
 mkdir ${TEMP_DIRECTORY}
 harp compile -o ${TEMP_DIRECTORY} || exit 1
+
+echo "Tuning .gitinore files to select which files to push into gh-pages"
 cp .gitignore ${TEMP_DIRECTORY}
+
+# Ignore .gitignore itself
 echo .gitignore >> ${TEMP_DIRECTORY}/.gitignore
+
+# Do not ignore generated files in /assets
+rm ${TEMP_DIRECTORY}/assets/.gitignore
 
 echo "Pushing into " ${ORIGIN_URL}
 cd ${TEMP_DIRECTORY}
@@ -25,8 +32,8 @@ git commit --allow-empty -m "Generated static site for $CURRENT_COMMIT" || exit 
 git remote add origin "$ORIGIN_URL_WITH_CREDENTIALS"
 git push -u --force origin gh-pages
 
-# echo "Cleaning up temp files"
-# rm -Rf ${TEMP_DIRECTORY}
+echo "Cleaning up temp files"
+rm -Rf ${TEMP_DIRECTORY}
 
 echo "Deployed successfully."
 exit 0
